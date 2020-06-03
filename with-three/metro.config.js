88c94ab978d7b05faff55c5bbea7245da7000d7d
Evan Bacon <baconbrix@gmail.com>
2020-06-03 15:13:35 -0700

    Update with-three dependencies

---
+++ with-three/metro.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  resolver: {
+    // Add all file asset extensions you plan on using to this list
+    // https://github.com/expo/expo-three#loading-assets
+    assetExts: ["db", "mp3", "ttf", "obj", "mtl", "dae", "png", "jpg"],
+  },
+};
